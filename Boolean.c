9827d552797271f4fef76998f27575c538b2e98d
MariselviB <114462425+MariselviB@users.noreply.github.com>
2022-09-29 16:16:59 +0530

    Create Boolean.c

---
+++ Boolean.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a=4,b=9;
+    {
+      printf("the output of bitwise and gate value :%d",a&b);
+      printf("\nthe output of bitwise or gate value:%d",a|b);
+      printf("\nthe output of bitwise exor gate value:%d",a^b);
+    }   
+    
+    return 0;
+}
